477e4ced9a7f8d18f811cbbb6e4ad7b74549469b
Matt A. Tobin <email@mattatobin.com>
2015-03-23 05:11:58 -0400

    Restructure Repo and Add more Git stuffs

--- defaults/preferences/defaults.js
+++
@@ -1,4 +0,0 @@
-pref("extensions.oam.browseAddons", "https://addons.mozilla.org/%LOCALE%/%APP%");
-pref("extensions.oam.getMoreExtensionsURL", "https://addons.mozilla.org/%LOCALE%/%APP%/extensions");
-pref("extensions.oam.getMoreThemesURL", "https://addons.mozilla.org/%LOCALE%/%APP%/themes");
-pref("extensions.oam.getMorePluginsURL", "https://addons.mozilla.org/%LOCALE%/%APP%/plugins");
